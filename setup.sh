3e6f58b32ef320085a99537e37546d56954c5473
Robin Hawkins <robin.hawkins@ultrahaptics.com>
2021-02-05 12:52:50 +0000

    Much-needed revampe of vimrc
    Replacing Vundle with Pathogen, installing some useful tools.

---
+++ setup.sh
@@ -0,0 +1,8 @@
+
+# Get pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle
+git clone git://github.com/tpope/vim-classpath.git
+git clone https://github.com/artur-shaik/vim-javacomplete2.git
