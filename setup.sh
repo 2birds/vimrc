022f2a93f5529d239e47aba03098394fbd388a1e
Robin <robinhshawkins@gmail.com>
2021-02-18 22:57:47 +0000

    Adding NERDTree

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle
+git clone https://github.com/preservim/nerdtree.git
 git clone git://github.com/tpope/vim-classpath.git
 git clone https://github.com/artur-shaik/vim-javacomplete2.git
